5c32f9a2aa84ed74b022a5dc154e9a9efa8499ae
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-04-23 16:58:36 +0200

    added position serialization

--- web/src/main.ts
+++ web/src/main.ts
@@ -12,15 +12,35 @@ const startingPosition = {
 
 const handsButtons = [
     [
-        document.getElementById("one-one"),
-        document.getElementById("one-two"),
+        document.getElementById('one-one'),
+        document.getElementById('one-two'),
     ],
     [
-        document.getElementById("two-one"),
-        document.getElementById("two-two"),
+        document.getElementById('two-one'),
+        document.getElementById('two-two'),
     ],
 ]
 
 const splitButtons = [
-    
+    document.getElementById('split')
 ]
+
+function positionToString(pos: Position): string {
+    return `${pos.turn} -> ${pos[0][0]}-${pos[0][1]} | ${pos[1][0]}-${pos[1][1]}`
+}
+
+function parsePosition(s: string): Position {
+    const [turn, rest] = s.split(' -> ')
+    const [one, two] = rest.split(' | ')
+
+    const [oneOne, oneTwo] = one.split('-')
+    const [twoOne, twoTwo] = one.split('-')
+
+    return {
+        turn: parseInt(turn),
+        players: [
+            [parseInt(oneOne), parseInt(oneTwo)],
+            [parseInt(twoOne), parseInt(twoTwo)],
+        ]
+    }
+}
